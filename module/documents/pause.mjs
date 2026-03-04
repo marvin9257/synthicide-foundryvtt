a4e622681f9b38a37c735ca2e8723ee37c93edb2
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2026-03-04 15:17:43 -0500

    fix: correct scope on document for pause

--- module/documents/pause.mjs
+++ module/documents/pause.mjs
@@ -1,3 +1,4 @@
+
 /**
  * Extend the GamePause application to use custom banner.
  * @extends {GamePause}
@@ -5,14 +6,14 @@
 export class SynthicideGamePause extends foundry.applications.ui.GamePause {
   /** @override */
   async _renderHTML(context, _options) {
-    const img = document.createElement("img");
+    const img = globalThis.document.createElement("img");
     img.src = "systems/synthicide/assets/synthicidePause.svg";
     if ( context.spin ) {
       img.classList.add("fa-spin");
     }
-    const caption = document.createElement("figcaption");
+    const caption = globalThis.document.createElement("figcaption");
     caption.innerText = context.text;
-    const figure = document.createElement("figure");
+    const figure = globalThis.document.createElement("figure");
     figure.appendChild(img);
     figure.appendChild(caption);
     return [figure, caption];
