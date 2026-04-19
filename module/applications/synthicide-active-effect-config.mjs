ae756f94a6c906c1861e8fd514fbbfe9828edeef
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2026-04-19 08:37:08 -0400

    chore: minor improvement to _renderChange
    Dont do change calcs if calling super

--- module/applications/synthicide-active-effect-config.mjs
+++ module/applications/synthicide-active-effect-config.mjs
@@ -12,19 +12,19 @@ export default class SynthicideActiveEffectConfig extends foundry.applications.s
 
   /** @override */
   async _renderChange(context) {
-    const { change, index } = context;
-    if ( typeof change.value !== "string" ) change.value = JSON.stringify(change.value);
-    Object.assign(
-      change,
-      ["key", "type", "value", "phase", "priority"].reduce((paths, fieldName) => {
-        paths[`${fieldName}Path`] = `system.changes.${index}.${fieldName}`;
-        return paths;
-      }, {})
-    );
-
     const parent = this.document?.parent;
     // If this AE is embedded on an Item of type `trait`, render our custom change partial
     if ( parent?.documentName === 'Item' && parent?.type === 'trait' ) {
+      //do data prep like super._renderChange
+      const { change, index } = context;
+      if ( typeof change.value !== "string" ) change.value = JSON.stringify(change.value);
+      Object.assign(
+        change,
+        ["key", "type", "value", "phase", "priority"].reduce((paths, fieldName) => {
+          paths[`${fieldName}Path`] = `system.changes.${index}.${fieldName}`;
+          return paths;
+        }, {})
+      );
       // Ensure the select options are available on the per-change context too
       if (!context.changeSelectOptions) context.changeSelectOptions = SYNTHICIDE.MODIFIER_TARGETS || {};
       return foundry.applications.handlebars.renderTemplate('systems/synthicide/templates/sheets/active-effect/change-synthicide.hbs', context);
